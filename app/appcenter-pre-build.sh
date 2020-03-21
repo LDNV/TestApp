56b10ec5f7d6f532c1450fa2ee1d88f07cb51de9
Benjamin Rangel <benjamin.rangel@mlb.com>
2020-03-20 18:16:28 -0600

    new

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -2,7 +2,7 @@
 
 echo "This is an App Center Pre-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
 
-for entry in /Users/*
+for entry in /Users/runner/runners/2.165.2/work/1/s/app/*
 do
   echo "$entry"
 done
