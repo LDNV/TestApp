ca27acab6cefee43fe9889b9314cb1c66e06cde8
Benjamin Rangel <benjamin.rangel@mlb.com>
2020-03-23 00:20:13 -0600

    login ?

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -6,5 +6,5 @@ for entry in /Users/runner/runners/2.165.2/work/1/s/app/build/outputs/apk/prod/r
 do
   echo "$entry"
 done
-
+appcenter login
 appcenter test run espresso --app "BenjaminRangel/TestApp" --devices 1dce183a --app-path "/app/build/outputs/apk/prod/release/app-prod-release.apk" --test-series "master" --locale "en_US" --build-dir "app/build/outputs/apk/androidTest/prod/debug"
