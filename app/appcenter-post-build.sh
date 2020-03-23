ee76d41f64128441121693bbf73e4dba8616478f
Benjamin Rangel <benjamin.rangel@mlb.com>
2020-03-23 15:20:58 -0600

    directory

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -2,7 +2,7 @@
 
 echo "Benjamin: This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
 
-for entry in /Users/runner/runners/2.165.2/work/1/s/app/build/outputs/apk/prod/release/*
+for entry in /Users/runner/runners/2.165.2/work/1/s/app/build/outputs/apk/*
 do
   echo "$entry"
 done
