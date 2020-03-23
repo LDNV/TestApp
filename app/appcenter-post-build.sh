6223cc063fab302982433745618c82e550a0a536
Benjamin Rangel <benjamin.rangel@mlb.com>
2020-03-22 20:43:02 -0600

    adding post build code

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-echo "This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+echo "Benjamin: This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+
+for entry in /Users/runner/runners/2.165.2/work/1/s/app/*
+do
+  echo "$entry"
+done
